0052efb050675fa945816bf038f110d83e9eae3e
luckkyyynew <154548770+luckkyyynew@users.noreply.github.com>
2023-12-22 09:54:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+  ['9233749771', 'luckys bot²⁰²³', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
